9d97cc3bbb5c2a96d94f2a89f9c29fd70540c75b
Haja Trawally <hajatrawally6@gmail.com>
2018-08-08 19:49:25 +0000

    automatically backed up by learn

---
+++ array.rb
@@ -0,0 +1,8 @@
+sports = ["basketball","baseball","football"]
+counter= 1 
+
+sports.each do |sport|
+  puts "Sport #{counter}: #{sport}"
+  counter = counter + 1 
+end 
+sports.each
---
+++ project.rb
@@ -0,0 +1,5 @@
+def say_hello 
+  puts "Hello! What is your name?"
+end 
+say_hello
+
