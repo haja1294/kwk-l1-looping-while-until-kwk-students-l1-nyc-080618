44bb3135f395eb7e8348c39d41aa5bbe2522d72f
Haja Trawally <hajatrawally6@gmail.com>
2018-08-08 19:49:30 +0000

    automatically backed up by learn

--- project.rb
+++ project.rb
@@ -1,5 +1,44 @@
-def say_hello 
-  puts "Hello! What is your name?"
+def say_hello
+puts "Hello! What is your name?" 
 end 
 say_hello
+ name = gets.chomp 
+puts "Hello #{name}"
 
+puts "Which city do you want to visit ?"
+puts "Pick One:" 
+puts "New York"
+puts "Los Angeles"
+puts "Miami"
+puts "New Orleans"
+puts "North Dakota"
+
+city = gets.chomp.downcase
+
+if city == "new york"
+ puts "Pizza"
+ puts "Bagel"
+ puts "Hot Dog"
+ 
+elsif city == "los angeles"
+puts "Salad"
+puts "Sushi"
+puts "Burger"
+
+elsif city == "miami"
+puts "Pizza"
+puts "Taco"
+puts "Fish"
+
+elsif city == "new orleans"
+puts "Gumbo"
+puts "Rice and Beans"
+puts "Steak"
+
+elsif city == "north dakota"
+puts "Grass"
+puts "Drive 90 miles to the nearest town with any food source"
+
+else
+ puts "Try Again"
+end
